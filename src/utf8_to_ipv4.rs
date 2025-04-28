a6ba1a73cfbe1d898ccc5a7ea18019821f59132c
Isaiah Sherfick <isaiahsherfick8@gmail.com>
2025-04-28 18:03:08 -0500

    fix unused result warning

--- src/utf8_to_ipv4.rs
+++ src/utf8_to_ipv4.rs
@@ -4,11 +4,11 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
-        writeln!(io::stdout(),"{}",String::from("0.0.0.0"));
+        let _ = writeln!(io::stdout(),"{}",String::from("0.0.0.0"));
         return;
     }
 
     let input = args[1].clone();
 
-    writeln!(io::stdout(),"{}",utf8_to_ipv4(&input));
+    let _ = writeln!(io::stdout(),"{}",utf8_to_ipv4(&input));
 }
